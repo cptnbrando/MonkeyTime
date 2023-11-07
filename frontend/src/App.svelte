e401f41359218c5114a77212fb92639537da828c
Brandooo! <becruz1738@gmail.com>
2023-11-07 15:36:44 -0600

    Added Disconnect Logic
    Callback that uses the working iceStateChanged event to remove connections once they are disconnected

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,7 +2,7 @@
   import { onMount } from "svelte";
   import Chat from "./lib/Chat.svelte";
   import { Web5 } from "@web5/api";
-  import { Peer } from "peerjs";
+  import { DataConnection, Peer } from "peerjs";
 
   let web5: Web5;
   let myDid: string;
@@ -26,6 +26,7 @@
     });
 
     let peerID = "";
+    let reconnected: boolean = false;
 
     // If there's an ID for this DiD already on the server
     // We get back that ID and set it for the Peer connection
@@ -36,12 +37,16 @@
 
     // If there's no ID for this DiD on the server
     // We get a new PeerID, so that way its populated for later checks
+    // Otherwise, a PeerID exists on the server (user might have opened 2 tabs)
+    // So we set reconnected flag to true
     if (peerID === "") {
       await fetch("/peerjs/monkeys/peerjs/id")
         .then((res) => res.text())
         .then((data) => {
           peerID = data;
         });
+    } else {
+      reconnected = true;
     }
 
     peer = new Peer(peerID, {
@@ -50,7 +55,7 @@
       path: "/peerjs/monkeys",
     });
 
-    const setIDs = await fetch("/setIDs", {
+    await fetch("/setIDs", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -66,27 +71,13 @@
     // If so, we can use that same id, or avoid a connection...
     peer.on("open", function (id) {
       console.log("ID: " + id);
-      // After opening the connection, we retrieve the list of IDs of all active Peers on the server
-      // Then we connect to all peerIDs except our own peerID
-      // fetch("/peerjs/monkeys/peerjs/peers")
-      //   .then(response => console.log(response));
-      // .then(data => {
-      //   console.log(data);
-      //   // data.forEach((element: any) => {
-      //   //   console.log("Attempting to connect to: " + element);
-      //   //   if(element !== peerID) {
-      //   //     peer.connect(element, {
-      //   //       metadata: {
-      //   //         did: myDid
-      //   //       }
-      //   //     });
-      //   //   }
-      //   // });
-      // });
     });
 
     peer.on("connection", function (c) {
       console.log("Connected to: " + c.peer);
+      c.on("iceStateChanged", (state) => {
+        disconnectFromPeer(state, c);
+      });
     });
 
     peer.on("disconnected", function () {
@@ -97,6 +88,7 @@
 
     peer.on("close", function () {
       console.log("Connection destroyed");
+      peer.destroy();
     });
 
     peer.on("error", function (err) {
@@ -106,20 +98,56 @@
 
     // Once the event handlers have been set, we connect to all peers found on the server
     fetch("/peerjs/monkeys/peerjs/peers")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((element: any) => {
-        if (element !== peerID) {
-          console.log("Attempting to connect to: " + element);
-          peer.connect(element, {
-            metadata: {
-              did: myDid,
-            },
-          });
-        }
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((element: any) => {
+          if (element !== peerID) {
+            console.log("Attempting to connect to: " + element);
+            const newConnection = connectToPeer(element);
+            newConnection.on("iceStateChanged", (state) => {
+              disconnectFromPeer(state, newConnection);
+            });
+          }
+        });
       });
-    });
   });
+
+  /**
+   * This if for a iceStateChanged callback, as the other events aren't working right now
+   * This is slow, but in the future we can change the callbacks to use the faster events
+   * For now, this removes the connection from the connections map, but keeps the id
+   * This could be used to reconnect easily in the future, and I can't get it to remove it completely
+   * For now, this works
+   * @param state an RTCIceConnectionState
+   * @param connection a DataConnection to close
+   */
+  function disconnectFromPeer(state: RTCIceConnectionState, connection: DataConnection) {
+    if (state !== "disconnected") return;
+
+    console.log("peer disconnected");
+    console.log(connection);
+
+    connection.close({
+      flush: true
+    });
+    peer._removeConnection(connection);
+    // const all = peer.connections as Map<string, Array<DataConnection>>;
+    // console.log(all);
+    // all.delete(connection.peer);
+  }
+
+  /**
+   * Connect to a peer, and supply the DiD as metadata
+   * The metadata can be changed to use our DwN monkey data
+   * @param peerID a peerID string to connect to
+   */
+  function connectToPeer(peerID: string): DataConnection {
+    return peer.connect(peerID, {
+      metadata: {
+        did: myDid,
+      },
+    });
+  }
 </script>
 
 <main>
